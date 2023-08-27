d3a1c89f494e97ff1da01516a64b1db0efe871bf
jazilhashmi <138767638+JaziBro@users.noreply.github.com>
2023-08-27 23:14:41 +0500

    message

--- calculate.js
+++ calculate.js
@@ -1,6 +1,8 @@
 import { sum } from "./add.js";
-import inquirer from "inquirer";
 import { sum2 } from "./subtract.js";
+import { sum3 } from "./multiply.js";
+import { sum4 } from "./divide.js";
+import inquirer from "inquirer";
 const answers = await inquirer.prompt([
     {
         name: "num1",
@@ -19,7 +21,19 @@ const answers = await inquirer.prompt([
         type: "list"
     }
 ]);
-const result = sum(answers.num1, answers.num2);
-console.log(`result is ${result}`);
-const result2 = sum2(answers.num1, answers.num2);
-console.log(`result is ${result2}`);
+if (answers.operator === "+") {
+    const result = sum(answers.num1, answers.num2);
+    console.log(`result is ${result}`);
+}
+else if (answers.operator === "-") {
+    const result = sum2(answers.num1, answers.num2);
+    console.log(`result is ${result}`);
+}
+else if (answers.operator === "*") {
+    const result = sum3(answers.num1, answers.num2);
+    console.log(`result is ${result}`);
+}
+else if (answers.operator === "/") {
+    const result = sum4(answers.num1, answers.num2);
+    console.log(`result is ${result}`);
+}
